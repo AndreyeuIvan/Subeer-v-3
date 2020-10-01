d3e9d617ae903369e0e499101545965d3377b0f0
AndreyeuIvan <andreeviv@icloud.com>
2020-10-01 23:45:42 +0300

    [template error fixed, add logout]

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
 
 from account import views as accounts_views
 
 
 urlpatterns = [
 	re_path(r'^signup/$', accounts_views.signup, name='signup'),
-    
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout')
 
 ]
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGOUT_REDIRECT_URL = 'list'
