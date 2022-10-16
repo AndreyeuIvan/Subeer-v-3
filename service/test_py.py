57caf800153987c18092ef282682ffdf7c896740
AndreyeuIvan <andreeviv@icloud.com>
2022-10-16 17:52:18 +0300

    parser enabled

--- service/test_py.py
+++ service/test_py.py
@@ -1,2 +1,11 @@
+import re
+import json
+
 with open('test.srt') as f:
-    print(f.read())
+    text = f.read()
+
+re_time_sub = r"(?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}\W+[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\n(?P<text>^\D*)"
+matches = re.finditer(re_time_sub, text, re.MULTILINE)
+data = [{i.groups()[0]:i.groups()[1]} for i in matches]
+
+json_data = json.dumps(data)
